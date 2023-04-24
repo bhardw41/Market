78afa3a0a5979de72ba73fa61da6d426e7dd50fc
bhardw41 <117127905+bhardw41@users.noreply.github.com>
2023-04-24 13:45:10 -0400

    Create utility.cpp

---
+++ utility.cpp
@@ -0,0 +1,12 @@
+#include "utility.hpp"
+// Order::Order(const Order& input){
+//   side=input.side;
+//   asset = input.asset;
+//   username = input.username;
+//   amount = input.amount;
+//   price = input.price;
+// }
+
+Order::Order()
+{ 
+}
